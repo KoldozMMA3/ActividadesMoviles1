70463f9efc7480171258b6bc0a6e25671d497d8d
Cristian Luna <128846559+KoldozMMA3@users.noreply.github.com>
2025-03-16 21:38:19 -0500

    Caso 2 Resuelto
    Mantuve la logica que tenia en mente y pude realizar un buen trabajo con el juego de papel, piedra y tijera

--- src/caso2.kt
+++ src/caso2.kt
@@ -2,8 +2,25 @@ import kotlin.random.Random
 
 fun main() {
     val opciones = listOf("Papel","Tijera","Piedra")
-    val IA  = Random.nextInt(1, opciones.size)
+    val IA  = opciones[Random.nextInt(3)]
 
     println("Elija entre las 3 opciones: $opciones")
-    var Humano = readLine()!!.toInt()
+    var Humano = readlnOrNull()
+
+    if (Humano !in opciones) {
+        println("Opcion Incorrecta")
+        return
+    }
+
+    println("IA eligio $IA")
+
+    if (Humano ==IA){
+        println("Empate")
+    } else if (Humano == "Papel" && IA == "Piedra" || Humano == "Tijera" && IA == "Papel" ||Humano == "Piedra" && IA == "Tijera" ){
+        println("Ganaste Humano")
+    } else {
+        println("IA Gano")
+    }
+
+
 }
